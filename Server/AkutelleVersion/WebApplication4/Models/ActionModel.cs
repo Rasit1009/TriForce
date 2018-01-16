5dcd635de3fd67c5123d219257bcfc238a150dfd
unknown <j-teutenberg@t-online.de>
2018-01-16 15:59:31 +0100

    Transaktiondb

---
+++ Server/AkutelleVersion/WebApplication4/Models/ActionModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Models
+{
+    public class Action
+    {
+        public string Actionid { get; set; }
+        public string Selleri { get; set; }
+        public string Useri { get; set; }
+
+        public int Sales{ get; set; }
+
+        public string Date { get; set; }
+    }
+}
