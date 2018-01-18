d5afa354af8cf82fa25630ce405a4b40a491d6f6
fried.fritz@gmail.com <fried.fritz@gmail.com>
2018-01-18 10:16:24 +0100

    completion_value seller

--- Dashboard/src/app/pages/datacomplete_seller/datacomplete_seller.component.ts
+++ Dashboard/src/app/pages/datacomplete_seller/datacomplete_seller.component.ts
@@ -16,6 +16,56 @@ const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
 
 export class Datacomplete_SellerComponent {
 
+  person : Person = this.auth.person;
+  completion_value : number = 0;
+
+
+  constructor(public auth : AuthService){
+    this.auth.isPersonSource.subscribe(res => {
+      this.person = res;
+      try {
+        if(this.person.businessname){
+          console.log(this.person.businessname);
+          this.completion_value = this.completion_value + 10;
+        }
+        if(this.person.firstname){
+          console.log(this.person.firstname);
+          this.completion_value = this.completion_value + 10;
+        }
+        if(this.person.lastname){
+          console.log(this.person.lastname);
+          this.completion_value = this.completion_value + 10;
+        }
+        if(this.person.street && this.person.housenumber){
+          console.log(this.person.street);
+          console.log(this.person.housenumber);
+          this.completion_value = this.completion_value + 10;
+        }
+        if(this.person.city && this.person.plz){
+          console.log(this.person.city);
+          console.log(this.person.plz);
+          this.completion_value = this.completion_value + 10;
+        }
+        if(this.person.business && this.person.business != "Branche..."){
+          console.log(this.person.business);
+          this.completion_value = this.completion_value + 10;
+        }
+        if(this.person.text){
+          console.log(this.person.text);
+          this.completion_value = this.completion_value + 10;
+        }
+        if(this.person.imagepath){
+          console.log(this.person.imagepath);
+          this.completion_value = this.completion_value + 30;
+        }
+        console.log(this.person.i);
+      } catch (error) {
+        console.log("noch kein user da "); 
+      }
+    });
+  }
+
+ /* Bilduploader 
   public uploader:FileUploader = new FileUploader({url: URL});
   public hasBaseDropZoneOver:boolean = false;
   public hasAnotherDropZoneOver:boolean = false;
@@ -27,7 +77,7 @@ export class Datacomplete_SellerComponent {
   public fileOverAnother(e:any):void {
     this.hasAnotherDropZoneOver = e;
   }
-
+*/
   
 }
 
