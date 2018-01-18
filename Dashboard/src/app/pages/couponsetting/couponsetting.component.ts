4e196b42aaeabdcdb9148b84068871fc483552e4
Malte <malte.hendricks@gmx.de>
2018-01-18 16:41:31 +0100

    modal umgelegt

--- Dashboard/src/app/pages/couponsetting/couponsetting.component.ts
+++ Dashboard/src/app/pages/couponsetting/couponsetting.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
 import { CouponService, Coupon } from '../coupon/coupon.service';
 import { Person } from '../datacomplete_consumer/services/person.service';
+import { ToasterService, ToasterConfig, Toast, BodyOutputType } from 'angular2-toaster';
+
+import 'style-loader!angular2-toaster/toaster.css';
 
 @Component({
   selector: 'app-couponsetting',
@@ -12,9 +15,12 @@ export class CouponsettingComponent implements OnInit {
 
   person : Person = this.auth.person; 
   coupon : Coupon = new Coupon(null,null,null,null,null);
-  constructor(public auth : AuthService, public couponService : CouponService) {
+
+  
+  constructor(public auth : AuthService, public couponService : CouponService, private toasterService: ToasterService) {
     this.couponService.getCoupon(this.person.i).subscribe(res => {
     this.coupon = res; 
+   // if(!this.coupon.moneyvalue )
     });
    }
 
@@ -51,5 +57,73 @@ export class CouponsettingComponent implements OnInit {
 
     this.couponService.sendSystem(this.coupon).subscribe();
   }
+
+
+
+  //---------------------------------------------TOAST START----------------------------------------------------
+datenaktualisieren: boolean = true;
+
+
+initToasts(){
+  //function call delay for fade-in effect mhendric 18.01.18
+
+  if(this.datenaktualisieren){
+    setTimeout(() => { this.showToast('sucess', 'Aktualisieren', 'Hier können Sie für eine bessere Kundenbindung Ihre Daten aktualisieren.');}, 1000);
+  } 
+  
+    }
+
+config: ToasterConfig;
+
+position = 'toast-top-right';
+animationType = 'flyleft';
+title = 'HI there!';
+content = `I'm cool toaster!`;
+timeout = 0;
+toastsLimit = 5;
+type = 'success';
+
+isNewestOnTop = false;
+isHideOnClick = true;
+isDuplicatesPrevented = false;
+isCloseButton = true;
+
+types: string[] = ['info', 'success', 'warning', 'error'];
+animations: string[] = ['fade', 'flyLeft', 'flyRight', 'slideDown', 'slideUp'];
+positions: string[] = ['toast-top-full-width', 'toast-bottom-full-width', 'toast-top-left', 'toast-top-center',
+  'toast-top-right', 'toast-bottom-right', 'toast-bottom-center', 'toast-bottom-left', 'toast-center'];
+
+
+makeToast() {
+  this.showToast(this.type, this.title, this.content);
+}
+
+
+public showToast(type: string, title: string, body: string) {
+  this.config = new ToasterConfig({
+    positionClass: this.position,
+    timeout: this.timeout,
+    newestOnTop: this.isNewestOnTop,
+    tapToDismiss: this.isHideOnClick,
+    preventDuplicates: this.isDuplicatesPrevented,
+    animation: this.animationType,
+    limit: this.toastsLimit,
+  });
+  const toast: Toast = {
+    type: type,
+    title: title,
+    body: body,
+    timeout: this.timeout,
+    showCloseButton: this.isCloseButton,
+    bodyOutputType: BodyOutputType.TrustedHtml,
+  };
+  this.toasterService.popAsync(toast);
+}
+
+clearToasts() {
+  this.toasterService.clear();
+}
+
+//---------------------------------------------TOAST END----------------------------------------------------
 
 }
--- Dashboard/src/app/pages/couponsetting/couponsetting.module.ts
+++ Dashboard/src/app/pages/couponsetting/couponsetting.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CouponsettingComponent } from './couponsetting.component';
 import { ThemeModule } from '../../@theme/theme.module';
+import { ToasterModule } from 'angular2-toaster';
 
 @NgModule({
   imports: [
     CommonModule,
     ThemeModule,
+    ToasterModule
   ],
   declarations: [CouponsettingComponent],
   exports: [
