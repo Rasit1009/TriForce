beb636b476b7b2218addcfb8b13a2ea2d391e75e
Malte <malte.hendricks@gmx.de>
2018-01-18 13:08:36 +0100

    toast weiter

--- Dashboard/src/app/pages/datacomplete_seller/datacomplete_seller.component.ts
+++ Dashboard/src/app/pages/datacomplete_seller/datacomplete_seller.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { Person } from '../datacomplete_consumer/services/person.service';
 import { AuthService } from '../../auth/auth.service';
 import { FileUploader } from 'ng2-file-upload';
+import { ToasterService, ToasterConfig, Toast, BodyOutputType } from 'angular2-toaster';
+
+import 'style-loader!angular2-toaster/toaster.css';
 
 // const URL = '/api/';
 const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
@@ -20,7 +23,7 @@ export class Datacomplete_SellerComponent {
   completion_value : number = 0;
 
 
-  constructor(public auth : AuthService){
+  constructor(private toasterService: ToasterService, public auth : AuthService){
     this.auth.isPersonSource.subscribe(res => {
       this.person = res;
       try {
@@ -63,6 +66,8 @@ export class Datacomplete_SellerComponent {
         console.log("noch kein user da "); 
       }
     });
+
+    this.initToasts();
   }
 
  /* Bilduploader 
@@ -79,5 +84,72 @@ export class Datacomplete_SellerComponent {
   }
 */
   
+
+//---------------------------------------------TOAST START----------------------------------------------------
+datenaktualisieren: boolean = true;
+
+
+  initToasts(){
+    //function call delay for fade-in effect mhendric 18.01.18
+
+    if(this.datenaktualisieren){
+      setTimeout(() => { this.showToast('sucess', 'Aktualisieren', 'Hier können Sie für eine bessere Kundenbindung Ihre Daten aktualisieren.');}, 1000);
+    } 
+    
+      }
+
+  config: ToasterConfig;
+
+  position = 'toast-top-right';
+  animationType = 'flyleft';
+  title = 'HI there!';
+  content = `I'm cool toaster!`;
+  timeout = 0;
+  toastsLimit = 5;
+  type = 'success';
+
+  isNewestOnTop = false;
+  isHideOnClick = true;
+  isDuplicatesPrevented = false;
+  isCloseButton = true;
+
+  types: string[] = ['info', 'success', 'warning', 'error'];
+  animations: string[] = ['fade', 'flyLeft', 'flyRight', 'slideDown', 'slideUp'];
+  positions: string[] = ['toast-top-full-width', 'toast-bottom-full-width', 'toast-top-left', 'toast-top-center',
+    'toast-top-right', 'toast-bottom-right', 'toast-bottom-center', 'toast-bottom-left', 'toast-center'];
+
+
+  makeToast() {
+    this.showToast(this.type, this.title, this.content);
+  }
+
+
+  public showToast(type: string, title: string, body: string) {
+    this.config = new ToasterConfig({
+      positionClass: this.position,
+      timeout: this.timeout,
+      newestOnTop: this.isNewestOnTop,
+      tapToDismiss: this.isHideOnClick,
+      preventDuplicates: this.isDuplicatesPrevented,
+      animation: this.animationType,
+      limit: this.toastsLimit,
+    });
+    const toast: Toast = {
+      type: type,
+      title: title,
+      body: body,
+      timeout: this.timeout,
+      showCloseButton: this.isCloseButton,
+      bodyOutputType: BodyOutputType.TrustedHtml,
+    };
+    this.toasterService.popAsync(toast);
+  }
+
+  clearToasts() {
+    this.toasterService.clear();
+  }
+
+//---------------------------------------------TOAST END----------------------------------------------------
+
 }
 
