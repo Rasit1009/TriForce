d56301be80b040dce5e96d4a99ad4ddad188773f
unknown <j-teutenberg@t-online.de>
2018-01-23 15:50:22 +0100

    ActionModel

---
+++ Server/AkutelleVersion/WebApplication4/Migrations/20180123143657_f3.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace WebApplication4.Migrations
+{
+    public partial class f3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Action",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Wochentag",
+                table: "Action",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Wochentag",
+                table: "Action");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Date",
+                table: "Action",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+        }
+    }
+}
