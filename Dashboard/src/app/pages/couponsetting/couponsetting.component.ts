3e5a823592534487491b1d4105868b320182f126
Malte <malte.hendricks@gmx.de>
2018-01-24 13:48:59 +0100

    toastcoupon

--- Dashboard/src/app/pages/couponsetting/couponsetting.component.ts
+++ Dashboard/src/app/pages/couponsetting/couponsetting.component.ts
@@ -18,7 +18,10 @@ export class CouponsettingComponent implements OnInit {
 
   
   
-  constructor(public auth : AuthService, public couponService : CouponService) {
+  constructor(public auth : AuthService, public couponService : CouponService, public toasterService: ToasterService) {
+
+this.showToast('sucess', 'test', 'test');
+
 
     this.auth.isPersonSource.subscribe(res => {
       this.person = res;
@@ -96,6 +99,78 @@ export class CouponsettingComponent implements OnInit {
 
     this.couponService.sendSystem(this.coupon).subscribe(()=>alert("geht klar"));
   }
+
+
+
+
+    //---------------------------------------------TOAST START----------------------------------------------------
+    datennochnichtvollstaendig: boolean = true;
+    nochnichtirgendwoeingekauft: boolean = true;
+    gutscheinverfuegbar: boolean = true;
+    ersteanmeldung: boolean = true;
+    
+    
+    
+      initToasts(){
+        //function call delay for fade-in effect mhendric 18.01.18
+        if(this.ersteanmeldung){
+          setTimeout(() => { this.showToast('sucess', 'Willkommen im Scanner', 'Hier kannst du LoloCOINs gutschreiben und LoloCOUPONs einlösen.');}, 1000);
+        } 
+        
+          }
+    
+      config: ToasterConfig;
+    
+      position = 'toast-bottom-right';
+      animationType = 'flyleft';
+      title = 'HI there!';
+      content = `I'm cool toaster!`;
+      timeout = 0;
+      toastsLimit = 5;
+      type = 'success';
+    
+      isNewestOnTop = false;
+      isHideOnClick = true;
+      isDuplicatesPrevented = false;
+      isCloseButton = true;
+    
+      types: string[] = ['info', 'success', 'warning', 'error'];
+      animations: string[] = ['fade', 'flyLeft', 'flyRight', 'slideDown', 'slideUp'];
+      positions: string[] = ['toast-top-full-width', 'toast-bottom-full-width', 'toast-top-left', 'toast-top-center',
+      'toast-top-right', 'toast-bottom-right', 'toast-bottom-center', 'toast-bottom-left', 'toast-center'];
+    
+    
+      makeToast() {
+        this.showToast(this.type, this.title, this.content);
+      }
+    
+    
+      public showToast(type: string, title: string, body: string) {
+        this.config = new ToasterConfig({
+          positionClass: this.position,
+          timeout: this.timeout,
+          newestOnTop: this.isNewestOnTop,
+          tapToDismiss: this.isHideOnClick,
+          preventDuplicates: this.isDuplicatesPrevented,
+          animation: this.animationType,
+          limit: this.toastsLimit,
+        });
+        const toast: Toast = {
+          type: type,
+          title: title,
+          body: body,
+          timeout: this.timeout,
+          showCloseButton: this.isCloseButton,
+          bodyOutputType: BodyOutputType.TrustedHtml,
+        };
+        this.toasterService.popAsync(toast);
+      }
+    
+      clearToasts() {
+        this.toasterService.clear();
+      }
+    
+    //---------------------------------------------TOAST END----------------------------------------------------
 
 
 }
